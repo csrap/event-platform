763b84976dfe61c2636a07b5c4cf2dc2f0b18c88
César Pinto <63161873+csrap@users.noreply.github.com>
2022-06-24 19:18:00 +0200

    Rotamento e player da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Header } from "./components/header";
 import { Event } from "./pages/Event";
-
+import { ApolloProvider } from '@apollo/client';
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 return (
   <div>
-    <Event/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+      <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   </div>
 )
 }
