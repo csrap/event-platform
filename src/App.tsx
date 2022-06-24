de877a670ab972ee8b1deccc0b0cc798774110b6
César Pinto <63161873+csrap@users.noreply.github.com>
2022-06-24 12:50:42 +0200

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { useEffect } from "react"
-import { gql, useQuery } from "@apollo/client"
+import { Header } from "./components/header";
+import { Event } from "./pages/Event";
 
 
-const GET_LESSONS_QUERY = gql`
-    query {
-      lessons {
-        id
-        title
-      }
-    }
-`
-
-interface Lesson {
-  id: string; 
-  title: string; 
-}
 function App() {
-const {data} = useQuery< { lessons:Lesson[]}>(GET_LESSONS_QUERY)
-
-
-  return (
-  <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title} </li>
-    })}
-  </ul>
-  )
+return (
+  <div>
+    <Event/>
+  </div>
+)
 }
 
 export default App
