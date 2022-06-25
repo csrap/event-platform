3196913ef3250a7028e8809b8b79e11a2e60f1c3
César Pinto <63161873+csrap@users.noreply.github.com>
2022-06-25 16:15:49 +0200

    Subscriber with GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+
+
 export const client = new ApolloClient(
   {
-    uri: 'https://api-us-west-2.graphcms.com/v2/cl4ocgefh1qfd01xr3v0phseg/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+      'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
   })
